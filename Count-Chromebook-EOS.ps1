2e4deea91f7c9617dae0ddcd1c03ce3db62d386b
Gabriel Clifton <46380574+GuruGabe@users.noreply.github.com>
2022-04-06 09:08:11 -0500

    Update Count-Chromebook-EOS.ps1
    Added list of Google OUs
    Added list of staff groups
    Added list of student groups
    Added Chromebook battery health export
    
    To get battery health export, changes must be made in Admin Console
    The settings are located here:
    
    Devices > Chrome > Settings > Devices > Device settings ~ User and device reporting
    
    Report device hardware information
    Navigate to Device settings.
    In the organizational units list, select the OU to apply the setting to. This setting can be set at the Root or for specific OUs.
    Scroll to the User and device reporting section.
    For the Report device Hardware Information setting, select Enable all hardware information reporting.
    Click Save.
    Caution: It is possible to select Customize for the Report device Hardware Information setting. With that selected, you also need to check Power status and Storage status in the customized list.
    
    Report device telemetry
    Navigate to Device settings.
    In the organizational units list, select the OU to apply the setting to. This setting can be set at the Root or for specific OUs.
    Scroll to the User and device reporting section.
    For the Report device telemetry setting, select Enable all telemetry reporting.
    Click Save.

--- Count-Chromebook-EOS.ps1
+++ Count-Chromebook-EOS.ps1
@@ -1,13 +1,13 @@
-﻿Remove-item "$env:TEMP\Chromebook_OS_EOS.csv", "$env:TEMP\Chromebook_OS_EOS2.csv", "$env:TEMP\Chromebook_OS_EOS3.csv", "$env:TEMP\Chromebook_OS_EOS4.csv", "$env:TEMP\Chromebook_OS_EOS5.csv", "$env:TEMP\OUs.csv"
+Remove-item "$env:TEMP\Chromebook_OS_EOS.csv", "$env:TEMP\Chromebook_OS_EOS2.csv", "$env:TEMP\Chromebook_OS_EOS3.csv", "$env:TEMP\Chromebook_OS_EOS4.csv", "$env:TEMP\Chromebook_OS_EOS5.csv", "$env:TEMP\OUs.csv"
 clear-host
 
 # Use GAM to get a list of all district Chromebooks
-gam print cros status assetid location model serialnumber autoUpdateExpiration lastSync osVersion orgUnitPath lastKnownNetwork MacAddress recentusers listlimit 1 | Out-File "$env:TEMP\Chromebook_OS_EOS.csv"
+gam print cros status assetid location model serialnumber autoUpdateExpiration lastSync osVersion orgUnitPath lastKnownNetwork MacAddress recentusers listlimit 1 diskvolumereports showdvrsfp | Out-File "$env:TEMP\Chromebook_OS_EOS.csv"
 
-Import-Csv -Path "$env:TEMP\Chromebook_OS_EOS.csv" | Select-Object deviceId,status,annotatedAssetId,annotatedLocation,model,serialNumber,autoUpdateExpiration,lastSync,osVersion,orgUnitPath,macAddress,recentUsers.email,lastKnownNetwork.ipAddress,lastKnownNetwork.wanIpAddress | Export-Csv -Path "$env:TEMP\Chromebook_OS_EOS 1.csv" -NoTypeInformation
+Import-Csv -Path "$env:TEMP\Chromebook_OS_EOS.csv" | Select-Object deviceId,status,annotatedAssetId,annotatedLocation,model,serialNumber,autoUpdateExpiration,lastSync,osVersion,orgUnitPath,macAddress,recentUsers.email,lastKnownNetwork.ipAddress,lastKnownNetwork.wanIpAddress,diskVolumeReports.volumeInfo.0.storageFreePercentage | Export-Csv -Path "$env:TEMP\Chromebook_OS_EOS 1.csv" -NoTypeInformation
 
 # Take the Chromebooks CSV and strip out headers I do not want and seperate Active, disabled and deprovisioned into separate CSV files
-$CSVData = Import-Csv -Path "$env:TEMP\Chromebook_OS_EOS 1.csv" -Header deviceId,status,annotatedAssetId,annotatedLocation,model,serialNumber,autoUpdateExpiration,lastSync,osVersion,orgUnitPath,macAddress,recentUsers.email,lastKnownNetwork.ipAddress,lastKnownNetwork.wanIpAddress
+$CSVData = Import-Csv -Path "$env:TEMP\Chromebook_OS_EOS 1.csv" -Header deviceId,status,annotatedAssetId,annotatedLocation,model,serialNumber,autoUpdateExpiration,lastSync,osVersion,orgUnitPath,macAddress,recentUsers.email,lastKnownNetwork.ipAddress,lastKnownNetwork.wanIpAddress,diskVolumeReports.volumeInfo.0.storageFreePercentage
 
 $MatchingExportFile = "$env:TEMP\Chromebook_OS_EOS 3.csv"
 $NonMatchingExportFile = "$env:TEMP\Chromebook_OS_EOS 2.csv"
@@ -44,7 +44,7 @@ Else {Write-Host "No non-matching data to export!"}
     Export-Csv "$env:TEMP\Chromebook_OS_EOS 2.csv" -NoType
 
 
-$CSVData = Import-Csv -Path "$env:TEMP\Chromebook_OS_EOS 3.csv" -Header deviceId,status,annotatedAssetId,annotatedLocation,model,serialNumber,autoUpdateExpiration,lastSync,osVersion,orgUnitPath,macAddress,recentUsers.email,lastKnownNetwork.ipAddress,lastKnownNetwork.wanIpAddress
+$CSVData = Import-Csv -Path "$env:TEMP\Chromebook_OS_EOS 3.csv" -Header deviceId,status,annotatedAssetId,annotatedLocation,model,serialNumber,autoUpdateExpiration,lastSync,osVersion,orgUnitPath,macAddress,recentUsers.email,lastKnownNetwork.ipAddress,lastKnownNetwork.wanIpAddress,diskVolumeReports.volumeInfo.0.storageFreePercentage
 $MatchingExportFile = "$env:TEMP\Chromebook_OS_EOS 4.csv"
 $NonMatchingExportFile = "$env:TEMP\Chromebook_OS_EOS 5.csv"
 $MatchingData = @()
@@ -84,7 +84,25 @@ Else {Write-Host "No non-matching data to export!"}
     Sort-Object autoUpdateExpiration, serialNumber |
     Export-Csv "$env:TEMP\Chromebook_OS_EOS 5.csv" -NoType
 
+gam print orgs | Out-File "$env:TEMP\OUs.csv"
+(Import-Csv "$env:TEMP\OUs.csv") |
+    Export-Csv "$env:TEMP\OUs.csv" -NoType
+
+gam print groups domain fsisd.net roles owners members managers | Out-File "$env:TEMP\staff_groups.csv"
+(Import-Csv "$env:TEMP\staff_groups.csv") |
+    Export-Csv "$env:TEMP\staff_groups.csv" -NoType
+
+gam print groups domain student.fsisd.net roles owners managers | Out-File "$env:TEMP\student_groups.csv"
+(Import-Csv "$env:TEMP\student_groups.csv") |
+    Export-Csv "$env:TEMP\student_groups.csv" -NoType
+
+gam print crostelemetry fields serialnumber batterystatusreport | Out-File "$env:TEMP\cros-telemetry.csv"
+
 #upload all sheets into one Google sheet, separate tabs.
 gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\Chromebook_OS_EOS 2.csv" csvsheet id:<Sheet / Tab ID>
 gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\Chromebook_OS_EOS 5.csv" csvsheet id:<Sheet / Tab ID>
 gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\Chromebook_OS_EOS 4.csv" csvsheet id:<Sheet / Tab ID>
+gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\OUs.csv" csvsheet id:<Sheet / Tab ID>
+gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\staff_groups.csv" csvsheet id:<Sheet / Tab ID>
+gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\student_groups.csv" csvsheet id:<Sheet / Tab ID>
+gam user fsisd.gam@fsisd.net update drivefile <Drive File ID> newfilename "Chromebook OS EOS" localfile "$env:TEMP\cros-telemetry.csv" csvsheet id:<Sheet / Tab ID>
